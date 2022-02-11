9462bf2cbec1b24512c9976ed5b614cffbc08550
renato-Aspire-E5-574 <remontanari@live.com>
2022-02-11 19:53:09 -0300

    install Admin Lte

---
+++ app/Models/Tenant.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Tenant extends Model
+{
+    use HasFactory;
+
+    protected $table = 'tenants'; 
+
+    protected $fillable = [
+        'name',
+        'uuid',        
+    ];
+
+    /**
+     * Relacionamentos
+    */
+    public function users()
+    {
+        return $this->hasMany(User::class);
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'tenant_id',
     ];
 
     /**
@@ -41,4 +42,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * Relacionamentos
+    */
+    public function tenant()
+    {
+        return $this->belongsTo(Tenant::class);
+    }
 }
