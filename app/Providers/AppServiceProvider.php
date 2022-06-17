4222d63db5794178a1cf11d4c505844795396539
renato-Aspire-E5-574 <remontanari@live.com>
2022-06-16 22:44:59 -0300

    app service update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -42,8 +42,8 @@ class AppServiceProvider extends ServiceProvider
         Plan::observe(PlanObserver::class);
         Tenant::observe(TenantObserver::class);
 
-        $identify = app(ManangerTenant::class)->identify();
-
+        //$identify = app(ManangerTenant::class)->identify();
+ 
         //Categorias de Imóveis
         $catImoveis = DB::table('imoveis')->selectRaw('DISTINCT tipo')->get();
         View()->share('categoriasMenu', $catImoveis);
