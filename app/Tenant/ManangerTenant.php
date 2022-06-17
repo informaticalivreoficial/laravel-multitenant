6d52e6cb6b7d114932c945a8ec3f62ca21129759
renato-Aspire-E5-574 <remontanari@live.com>
2022-06-16 22:12:00 -0300

    app service update

--- app/Tenant/ManangerTenant.php
+++ app/Tenant/ManangerTenant.php
@@ -13,44 +13,44 @@ class ManangerTenant
     //     return $pieces[0];
     // }
 
-    // public function domain()
-    // {
-    //     //superimoveis.info
-    //     return $pieces = request()->getHost();       
-    // }
+    public function domain()
+    {
+        //superimoveis.info
+        return $pieces = request()->getHost();       
+    }
 
-    // public function tenant()
-    // {
-    //     // $subdominio = $this->subdomain();
-    //     // $tenant = Tenant::where('subdominio', $subdominio)->first();
-    //     $dominio = $this->domain();
-    //     $tenant = Tenant::where('dominio', $dominio)->first();
-    //     return $tenant;
-    // }
+    public function tenant()
+    {
+        // $subdominio = $this->subdomain();
+        // $tenant = Tenant::where('subdominio', $subdominio)->first();
+        $dominio = $this->domain();
+        $tenant = Tenant::where('dominio', $dominio)->first();
+        return $tenant;
+    }
 
-    // public function identify()
-    // {
-    //     $tenant = $this->tenant();
-    //     return $tenant->id;
-    // }
+    public function identify()
+    {
+        $tenant = $this->tenant();
+        return $tenant->id;
+    }
 
-    // public function getTenantIdentify()
-    // {
-    //     return auth()->check() ? auth()->user()->tenant_id : '';
-    // }
+    public function getTenantIdentify()
+    {
+        return auth()->check() ? auth()->user()->tenant_id : '';
+    }
 
-    // public function getTenant()
-    // {
-    //     return auth()->check() ? auth()->user()->tenant : '';
-    // }
+    public function getTenant()
+    {
+        return auth()->check() ? auth()->user()->tenant : '';
+    }
 
-    // public function isAdmin(): bool
-    // {
-    //     return in_array(auth()->user()->email, config('acl.admins'));
-    // }
+    public function isAdmin(): bool
+    {
+        return in_array(auth()->user()->email, config('acl.admins'));
+    }
 
-    // public function isDomainMain(): bool
-    // {
-    //     return in_array($this->tenant()->dominio, config('acl.domainMain'));
-    // }
+    public function isDomainMain(): bool
+    {
+        return in_array($this->tenant()->dominio, config('acl.domainMain'));
+    }
 }
