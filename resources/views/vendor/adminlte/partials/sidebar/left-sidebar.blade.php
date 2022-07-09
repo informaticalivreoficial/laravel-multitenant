ad0374f9526c7c1bd752f621ec08b6b8a4c4ae84
renato-Aspire-E5-574 <remontanari@live.com>
2022-07-09 09:35:12 -0300

    composer update

--- resources/views/vendor/adminlte/partials/sidebar/left-sidebar.blade.php
+++ resources/views/vendor/adminlte/partials/sidebar/left-sidebar.blade.php
@@ -4,12 +4,19 @@
     <a href="#" class="brand-link text-center navbar-light">                    
         <img width="{{env('LOGOMARCA_GERENCIADOR_WIDTH')}}" height="{{env('LOGOMARCA_GERENCIADOR_HEIGHT')}}" src="{{auth()->user()->tenant->getlogoadmin() ?? env('DESENVOLVEDOR_LOGO_ADMIN')}}" alt="{{auth()->user()->tenant->name ?? 'Super Imóveis'}}" class="elevation-3">
     </a>
-    
+    @php
+        if(!empty(\Illuminate\Support\Facades\Auth::user()->avatar) &&
+        \Illuminate\Support\Facades\File::exists(public_path() . '/storage/' . \Illuminate\Support\Facades\Auth::user()->avatar)){
+            $cover = \Illuminate\Support\Facades\Auth::user()->url_avatar;
+        } else {
+            $cover = url(asset('backend/assets/images/image.jpg'));
+        }
+    @endphp
     {{-- Sidebar menu --}}
     <div class="sidebar">
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
-                <img src="" class="img-circle elevation-2" alt="{{ \Illuminate\Support\Facades\Auth::user()->name }}">
+                <img src="{{$cover}}" class="img-circle elevation-2" alt="{{ \Illuminate\Support\Facades\Auth::user()->name }}">
             </div>
             <div class="info">
                 <a href="{{ route('users.edit', ['id' => \Illuminate\Support\Facades\Auth::user()->id] )}}" class="d-block">{{ \Illuminate\Support\Facades\Auth::user()->name }}</a>
