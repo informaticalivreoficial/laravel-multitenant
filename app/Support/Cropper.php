53ce81358ce51856fc29c6ddf398d73389fbef2f
renato-Aspire-E5-574 <remontanari@live.com>
2022-07-09 09:56:52 -0300

    cropper update

--- app/Support/Cropper.php
+++ app/Support/Cropper.php
@@ -8,12 +8,13 @@
 
 namespace App\Support;
 
+use CoffeeCode\Cropper\Cropper as CropperCropper;
 
 class Cropper
 {
     public static function thumb(string $uri, int $width, int $height = null)
     {
-        $cropper = new \CoffeeCode\Cropper\Cropper('../public/storage/cache');
+        $cropper = new CropperCropper('../public/storage/cache');
         $pathThumb = $cropper->make(config('filesystems.disks.public.root') . '/' . $uri, $width, $height);
 
         $file = 'cache/' . collect(explode('/', $pathThumb))->last();
@@ -22,7 +23,7 @@ class Cropper
 
     public static function flush(?string $path)
     {
-        $cropper = new \CoffeeCode\Cropper\Cropper('../public/storage/cache');
+        $cropper = new CropperCropper('../public/storage/cache');
 
         if(!empty($path)) {
             $cropper->flush($path);
