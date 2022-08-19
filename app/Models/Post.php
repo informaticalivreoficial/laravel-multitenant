1961dc03212d802279e695610531d3a790befcf0
renato-Aspire-E5-574 <remontanari@live.com>
2022-08-19 11:41:22 -0300

    s3 post path

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -94,11 +94,11 @@ class Post extends Model
             $cover = $images->first(['path']);
         }
 
-        if(empty($cover['path']) || !File::exists('../public/storage/' . $cover['path'])) {
+        if(empty($cover['path']) || !Storage::disk()->exists($cover['path'])) {
             return url(asset('backend/assets/images/image.jpg'));
         }
 
-        return Storage::url(Cropper::thumb($cover['path'], 720, 480));
+        return Storage::url($cover['path']);
     }
 
     public function nocover()
@@ -111,7 +111,7 @@ class Post extends Model
             $cover = $images->first(['path']);
         }
 
-        if(empty($cover['path']) || !File::exists('../public/storage/' . $cover['path'])) {
+        if(empty($cover['path']) || !Storage::disk()->exists($cover['path'])) {
             return url(asset('backend/assets/images/image.jpg'));
         }
 
