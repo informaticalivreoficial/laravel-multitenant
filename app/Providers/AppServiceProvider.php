f35cf9c4ab3b605b1d4775db27d91b06424e6ca7
informaticalivreoficial <renatomontanari@ymail.com>
2025-02-03 22:14:04 -0300

    update laravel 9 to 8

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -40,39 +40,39 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        URL::forceScheme('https'); 
-        Plan::observe(PlanObserver::class);
-        Tenant::observe(TenantObserver::class);
+        //URL::forceScheme('https'); 
+        // Plan::observe(PlanObserver::class);
+        // Tenant::observe(TenantObserver::class);
 
-        $identify = app(ManangerTenant::class)->identify();
+        // $identify = app(ManangerTenant::class)->identify();
  
-        //Categorias de Imóveis
-        $catImoveis = DB::table('imoveis')->selectRaw('DISTINCT tipo')->get();
-        View()->share('categoriasMenu', $catImoveis);
+        // //Categorias de Imóveis
+        // $catImoveis = DB::table('imoveis')->selectRaw('DISTINCT tipo')->get();
+        // View()->share('categoriasMenu', $catImoveis);
 
-        //Newsletter
-        $newsletter = NewsletterCat::where('sistema', 1)
-                        ->where('tenant_id', $identify)
-                        ->where('status', 1)
-                        ->get();
-        View()->share('newsletterForm', $newsletter);
+        // //Newsletter
+        // $newsletter = NewsletterCat::where('sistema', 1)
+        //                 ->where('tenant_id', $identify)
+        //                 ->where('status', 1)
+        //                 ->get();
+        // View()->share('newsletterForm', $newsletter);
 
-        //Lançamento
-        $lancamento = Imovel::where('destaque', 1)
-                        ->where('tenant_id', $identify)
-                        ->where('status', 1)
-                        ->get();
-        View()->share('lancamentoMenu', $lancamento);
-        //Páginas
-        $paginas = Post::where('tipo', 'pagina')
-                        ->where('tenant_id', $identify)
-                        ->where('status', 1)
-                        ->get();
-        View()->share('paginaMenu', $paginas);
+        // //Lançamento
+        // $lancamento = Imovel::where('destaque', 1)
+        //                 ->where('tenant_id', $identify)
+        //                 ->where('status', 1)
+        //                 ->get();
+        // View()->share('lancamentoMenu', $lancamento);
+        // //Páginas
+        // $paginas = Post::where('tipo', 'pagina')
+        //                 ->where('tenant_id', $identify)
+        //                 ->where('status', 1)
+        //                 ->get();
+        // View()->share('paginaMenu', $paginas);
 
-        Schema::defaultStringLength(191);
-        Blade::aliasComponent('admin.components.message', 'message');
+        // Schema::defaultStringLength(191);
+        // Blade::aliasComponent('admin.components.message', 'message');
 
-        Paginator::useBootstrap();
+        // Paginator::useBootstrap();
     }
 }
